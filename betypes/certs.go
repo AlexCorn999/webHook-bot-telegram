96882b30a4b6f74ade83a2765529df06fc56d5f2
AlexCorn999 <alexcorn999@list.ru>
2023-03-29 12:48:36 +0300

    fixed

--- betypes/certs.go
+++ betypes/certs.go
@@ -1,6 +1,6 @@
 package betypes
 
 const (
-	CERT_PATH = "/Users/alex_corn_/projectsGo/YouTube/webHook/certs/cert.crt"
-	KEY_PATH  = "/Users/alex_corn_/projectsGo/YouTube/webHook/certs/cert.key"
+	CERT_PATH = "/certs/cert.crt"
+	KEY_PATH  = "/certs/cert.key"
 )
